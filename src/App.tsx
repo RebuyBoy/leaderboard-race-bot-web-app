f0d38d276ee8159d2424221f601724049badcbd0
RebuyBoy <67625553+RebuyBoy@users.noreply.github.com>
2024-02-28 23:01:47 +0700

    test: receive data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import {Select, TextInput} from "@mantine/core";
 
 const App: React.FC = () => {
     const tg = window.Telegram.WebApp;
+    const [gameType, setGameType] = useState<string | null>('');
+    const [stake, setStake] = useState<string | null>('');
+    const gameTypes = ['game1', 'game2', 'game3'];
+    const stakes = ['stake1', 'stake2', 'stake3'];
+    const startParam = window.Telegram.WebApp.initDataUnsafe.start_param
 
     useEffect(() => {
         tg.ready();
-    }, [tg]);
+    });
 
     const onClose = () => {
         tg.sendData(JSON.stringify({cmd: 'close', user: tg.initDataUnsafe?.user?.first_name}));
@@ -17,8 +23,12 @@ const App: React.FC = () => {
         <div className="App">
             <header className="App-header">
                 <h1>Telegram Web App</h1>
+                <h2>start param {startParam}</h2>
                 <p>hello {tg.initDataUnsafe?.user?.first_name}</p>
-                <button onClick={onClose}>Close</button>
+                <Select data={gameTypes} value={gameType} onChange={setGameType}/>
+                <Select data={stakes} value={stake} onChange={setStake}/>
+                <TextInput placeholder="Your nickname"/>
+                <button onClick={onClose}>Subscribe</button>
             </header>
         </div>
     )
