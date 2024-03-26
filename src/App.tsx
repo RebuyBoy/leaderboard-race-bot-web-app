135caf134b71bc75d5e945c99b87451b33e1d2f7
RebuyBoy <67625553+RebuyBoy@users.noreply.github.com>
2024-03-27 03:59:21 +0700

    test: change to chat_id

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
                 "gameType": gameType,
                 "stake": stake,
                 "nickname": nickname,
-                "chat_id": tg.initDataUnsafe?.chat?.id
+                "chatId": tg.initDataUnsafe?.chat?.id
             }),
         });
         tg.close();
