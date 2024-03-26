4f572ff0641f563a96115c2b5ae270190d497a07
RebuyBoy <67625553+RebuyBoy@users.noreply.github.com>
2024-03-27 02:33:54 +0700

    test: receive data

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ const App: React.FC = () => {
     const tg = window.Telegram.WebApp;
     const [gameType, setGameType] = useState<string | null>('gggg');
     const [stake, setStake] = useState<string | null>('ssss');
-    const gameTypes = ['game1', 'game2', 'game3'];
-    const stakes = ['stake1', 'stake2', 'stake3'];
+    const [nickname, setNickname] = useState<string | null>('nnnn');
+    const gameTypes = ['SIT_GO_AOF', 'HOLDEM_9MAX', 'BATTLE_ROYALE'];
+    const stakes = ['S_100', 'S_200', 'S_300'];
 
     useEffect(() => {
         tg.ready();
@@ -20,8 +21,9 @@ const App: React.FC = () => {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                gameType,
-                stake
+                "gameType": gameType,
+                "stake": stake,
+                "nickname": nickname,
             }),
         });
         tg.close();
@@ -33,10 +35,12 @@ const App: React.FC = () => {
             <header className="App-header">
                 <h1>Telegram Web App</h1>
                 <h2>start param {tg.initDataUnsafe?.start_param || 'Error: start_param not found'}</h2>
-                <p>hello {tg.initDataUnsafe?.user?.first_name}</p>
+                <h2>start param {tg.initData || 'Error: initData not found'}</h2>
+                <p>hello: {tg.initDataUnsafe?.user?.first_name}</p>
+                <p>query id: {tg.initDataUnsafe?.query_id}</p>
                 <Select data={gameTypes} value={gameType} onChange={setGameType}/>
                 <Select data={stakes} value={stake} onChange={setStake}/>
-                <TextInput placeholder="Your nickname"/>
+                <TextInput onInput={() => setNickname} placeholder="Your nickname"/>
                 <button onClick={onClose}>Subscribe</button>
             </header>
         </div>
